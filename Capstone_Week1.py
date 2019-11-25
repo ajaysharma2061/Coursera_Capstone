60a48a42a993a03058a163287209ab76b49b74a4
Ajay Kumar <48777400+ajaysharma2061@users.noreply.github.com>
2019-11-25 22:11:05 +0800

    Add files via upload

---
+++ Capstone_Week1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 1. Write some markdown to explain that this notebook will be mainly used for the capstone project.
+
+
+# #Coursera Capstone Project
+# \Hey there! This is my Data Science Project\
+# ```python
+# print "Hello World"
+# ```
+# 
+# ```javascript
+# console.log("Hello World")
+# ```
+
+# In[2]:
+
+
+# 2. Import the pandas library as pd
+import pandas as pd
+
+
+# In[3]:
+
+
+# 3. Import the Numpy library as np
+import numpy as np
+
+
+# In[4]:
+
+
+# 4. Print the following the statement: Hello Capstone Project Course!
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
